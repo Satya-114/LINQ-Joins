35eeb89e132c406982604119fa672cf3b1c84877
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 16:02:28 +0530

    Add files via upload

---
+++ Left Join/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_LeftJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var QSOuterJoin = from emp in Employee.GetAllEmployees()
+                              join add in Address.GetAddress()
+                              on emp.AddressId equals add.ID
+                              into EmployeeAddressGroup
+                              from address in EmployeeAddressGroup.DefaultIfEmpty()
+                              select new { emp, address };
+
+            foreach (var item in QSOuterJoin)
+            {
+                Console.WriteLine($"Name : {item.emp.Name}, Address : {item.address?.AddressLine} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
