463a1f83de1234833d27f40d8488553911247ec1
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 16:00:38 +0530

    Add files via upload

---
+++ Inner Join MUL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Join_MUL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var JoinMultipleDSUsingQS = (//Data Source1
+                                         from emp in Employee.GetAllEmployees()
+                                             //Joining with Address Data Source (Data Source2)
+                                         join adrs in Address.GetAllAddresses()
+                                         on emp.AddressId equals adrs.ID
+                                         //Joining with Department Data Source (Data Source3)
+                                         join dept in Department.GetAllDepartments()
+                                         on emp.DepartmentId equals dept.ID
+                                         //Projecting the result set
+                                         select new
+                                         {
+                                             ID = emp.ID,
+                                             EmployeeName = emp.Name,
+                                             DepartmentName = dept.Name,
+                                             AddressLine = adrs.AddressLine
+                                         }).ToList();
+            foreach (var employee in JoinMultipleDSUsingQS)
+            {
+                Console.WriteLine($"ID = {employee.ID}, Name = {employee.EmployeeName}, Department = {employee.DepartmentName}, Addres = {employee.AddressLine}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
