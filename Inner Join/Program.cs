9eb4c18325cc93e991fb60ddb8ceb247b5a5c8c9
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 15:57:57 +0530

    Add files via upload

---
+++ Inner Join/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var JoinUsingMS = Employee.GetAllEmployees() //Outer Data Source
+                           .Join(
+                           Address.GetAllAddresses(),  //Inner Data Source
+                           employee => employee.AddressId, //Inner Key Selector
+                           address => address.ID, //Outer Key selector
+                           (employee, address) => new //Projecting the data into a result set
+                           {
+                               EmployeeName = employee.Name,
+                               AddressLine = address.AddressLine
+                           }).ToList();
+            foreach (var employee in JoinUsingMS)
+            {
+                Console.WriteLine($"Name :{employee.EmployeeName}, Address : {employee.AddressLine}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
