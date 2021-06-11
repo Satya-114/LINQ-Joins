79f960913ce96da77e55e2c1f7f2b9874da9c6a2
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 16:06:10 +0530

    Add files via upload

---
+++ Cross Join/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_CrossJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CrossJoinResult = from employee in Student.GetAllStudnets()
+                                  from subject in Subject.GetAllSubjects()
+                                  select new
+                                  {
+                                      Name = employee.Name,
+                                      SubjectName = subject.SubjectName
+                                  };
+            foreach (var item in CrossJoinResult)
+            {
+                Console.WriteLine($"Name : {item.Name}, Subject: {item.SubjectName}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cross Join/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_CrossJoin
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public static List<Student> GetAllStudnets()
+        {
+            return new List<Student>()
+            {
+                new Student { ID = 1, Name = "Preety"},
+                new Student { ID = 2, Name = "Priyanka"},
+                new Student { ID = 3, Name = "Anurag"},
+                new Student { ID = 4, Name = "Pranaya"},
+                new Student { ID = 5, Name = "Hina"}
+            };
+        }
+    }
+    public class Subject
+    {
+        public int ID { get; set; }
+        public string SubjectName { get; set; }
+        public static List<Subject> GetAllSubjects()
+        {
+            return new List<Subject>()
+            {
+                new Subject { ID = 1, SubjectName = "ASP.NET"},
+                new Subject { ID = 2, SubjectName = "SQL Server" },
+                new Subject { ID = 5, SubjectName = "Linq"}
+            };
+        }
+    }
+}
