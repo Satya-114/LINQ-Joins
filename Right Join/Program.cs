5dfa0360d21a58174d93890a437edc459e095d91
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 16:04:39 +0530

    Add files via upload

---
+++ Right Join/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_RightJoin
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var QSOuterJoin = from add in Address.GetAddress()
+                              join emp in Employee.GetAllEmployees()
+                              on add.ID equals emp.AddressId
+                              into EmployeeAddressGroup
+                              from emp in EmployeeAddressGroup.DefaultIfEmpty()
+                              select new { emp, add };
+
+            foreach (var item in QSOuterJoin)
+            {
+                Console.WriteLine($"Address : {item.add?.AddressLine},\t Name : {item.emp.Name} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
